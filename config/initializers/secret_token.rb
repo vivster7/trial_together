6037c49bde8055a8b07f2f47749c9aa9b0867873
Kenny <scooby@Scooby-THINKPAD.(none)>
2013-01-15 14:20:20 -0500

    setup directory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuTrial::Application.config.secret_token = '4f8b2b192fd84d7757dd19e530652e72e2040d233c3d5c95d73cac21ad6b9c4dc94c7c193016d5b93243dbfbc3758ea84067ba52ac055335fb292ef7ffa6fa9e'
