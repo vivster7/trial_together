1128c851c7133d46e6635a1cf42ccfeed6177e65
Vivek D <vivster7@gmail.com>
2013-01-16 18:37:15 -0500

    Added Title elements

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,26 +7,31 @@ describe "StaticPages" do
   describe "Home Page" do
   	before { visit root_path }
 
-  	it "should have the content 'Sample App'" do
-      page.should have_content('ClinicalTrials')
-    end
+  	it { should have_content('ClinicalTrials') }
+    it { should have_selector('title', text: "ClinicalTrials") }
   end
 
   describe "About Page" do
   	before { visit about_path }
 
   	it { should have_content('About Us') }
+    it { should have_selector('title', text: "ClinicalTrials | About") }
+
   end
 
   describe "Help Page" do
   	before { visit help_path }
 
   	it {should have_content('Help') }
+    it { should have_selector('title', text: "ClinicalTrials | Help") }
+
   end
 
   describe "Contact Page" do
   	before { visit contact_path }
 
   	it {should have_content('Contact Us') }
+    it { should have_selector('title', text: "ClinicalTrials | Contact") }
+
   end
 end
