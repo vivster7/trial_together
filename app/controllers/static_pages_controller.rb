65dbc846b9ac5056411854b95b3d34ac833cee2a
Vivek D <vivster7@gmail.com>
2013-01-16 18:16:57 -0500

    Added static pages help, contact, and about

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,15 @@
 class StaticPagesController < ApplicationController
+
+def home
+end
+
+def help
+end
+
+def about
+end
+
+def contact
+end
+
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,26 @@ describe "StaticPages" do
   describe "Home Page" do
   	before { visit root_path }
 
-  	it { should have_selector('h1', 	text: 'Sample App') }
-  	
+  	it "should have the content 'Sample App'" do
+      page.should have_content('ClinicalTrials')
+    end
+  end
+
+  describe "About Page" do
+  	before { visit about_path }
+
+  	it { should have_content('About Us') }
+  end
+
+  describe "Help Page" do
+  	before { visit help_path }
+
+  	it {should have_content('Help') }
+  end
+
+  describe "Contact Page" do
+  	before { visit contact_path }
+
+  	it {should have_content('Contact Us') }
   end
 end
