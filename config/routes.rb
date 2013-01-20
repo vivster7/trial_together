717c1ff090b2eac72482da0cae206a559260c117
Vivek D <vivster7@gmail.com>
2013-01-20 12:34:03 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 HerokuTrial::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
 
   root to: 'static_pages#home'
 
   match '/register', to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,10 +5,10 @@ describe "User pages" do
   subject { page }
 
   describe "signup page" do
-    before { visit signup_path }
+    before { visit register_path }
 
     it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: 'Sign up') }
+    it { should have_selector('title', text: 'Register') }
   end
 
   describe "profile page" do
@@ -29,6 +29,13 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Register') }
+        it { should have_content('error') }
+      end
     end
 
     describe "with valid information" do
@@ -41,6 +48,20 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
+      
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
       end
     end
   end
