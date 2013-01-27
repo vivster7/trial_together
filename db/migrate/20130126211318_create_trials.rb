99c70160b80ae0b613850a18c32822b250925bb7
Kenny <scooby@Scooby-THINKPAD.(none)>
2013-01-26 19:34:17 -0500

    moved brief_title over to text as well

--- db/migrate/20130126211318_create_trials.rb
+++ db/migrate/20130126211318_create_trials.rb
@@ -6,7 +6,7 @@ class CreateTrials < ActiveRecord::Migration
       t.string :org_study_id
       t.string :nct_id
       t.text :official_title
-      t.string :brief_title
+      t.text :brief_title
       t.text :brief_summary
       t.string :overall_status
       t.string :start_date
--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(:version => 20130126213010) do
     t.string   "org_study_id"
     t.string   "nct_id"
     t.text     "official_title"
-    t.string   "brief_title"
+    t.text     "brief_title"
     t.text     "brief_summary"
     t.string   "overall_status"
     t.string   "start_date"
